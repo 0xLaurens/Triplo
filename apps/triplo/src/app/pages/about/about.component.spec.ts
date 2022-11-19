09f2fb81838e42ffadd307bce6156d9684a7c1ef
lhh.weterings <lhh.weterings@student.avans.nl>
2022-11-19 21:45:27 +0100

    About-> test import fix

--- apps/triplo/src/app/pages/about/about.component.spec.ts
+++ apps/triplo/src/app/pages/about/about.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
+import {AboutProjectComponent} from "./about-project/about-project.component";
+import {AboutUserstoriesComponent} from "./about-userstories/about-userstories.component";
+import {AboutEntityComponent} from "./about-entity/about-entity.component";
 
 describe('AboutComponent', () => {
   let component: AboutComponent;
@@ -8,7 +11,7 @@ describe('AboutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AboutComponent],
+      declarations: [AboutComponent, AboutProjectComponent, AboutUserstoriesComponent, AboutEntityComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AboutComponent);
