c5dc8a4e21293d8a56d379d2b69c6e23ef8d9b2c
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-11-26 20:03:40 +0100

    TEST -> schema disable

--- apps/triplo/src/app/pages/project/project-card-list/project-card-list.component.spec.ts
+++ apps/triplo/src/app/pages/project/project-card-list/project-card-list.component.spec.ts
@@ -1,7 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { ProjectCardListComponent } from './project-card-list.component';
+import {ProjectCardListComponent} from './project-card-list.component';
 import {RouterTestingModule} from "@angular/router/testing";
+import {NO_ERRORS_SCHEMA} from "@angular/core";
 
 describe('ProjectCardListComponent', () => {
   let component: ProjectCardListComponent;
@@ -11,9 +12,9 @@ describe('ProjectCardListComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ProjectCardListComponent],
       imports: [RouterTestingModule],
+      schemas: [NO_ERRORS_SCHEMA],
       providers: []
     }).compileComponents();
-
     fixture = TestBed.createComponent(ProjectCardListComponent);
     component = fixture.componentInstance;
     component.project = {DislikeCount: 0, LikeCount: 0, _id: "asdf", created: Date.prototype, description: "", name: ""}
