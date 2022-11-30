589e29152d10d162dce309307e0ffcf82a832884
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-11-30 14:33:13 +0100

    COMMENT -> references added

--- apps/api/src/app/comment/comment.schema.ts
+++ apps/api/src/app/comment/comment.schema.ts
@@ -3,14 +3,22 @@ import mongoose from "mongoose";
 
 @Schema()
 export class Comment {
+  @Prop({type: mongoose.Schema.Types.ObjectId, ref: "Project"})
+  project: string;
   @Prop({})
   message: string;
+
+  @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'Comment'})
+  parent?: string
 
   @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'User'})
   owner: string;
 
   @Prop({})
   username: string;
+
+  @Prop({default: Date.now})
+  created: Date;
 
   @Prop({})
   LikeCount: number;
--- libs/models/src/lib/comment/comment.interface.ts
+++ libs/models/src/lib/comment/comment.interface.ts
@@ -1,9 +1,17 @@
-import {IsInt, IsMongoId, IsString, MaxLength, Min, MinLength} from "class-validator";
+import {IsDate, IsInt, IsMongoId, IsString, MaxLength, Min, MinLength} from "class-validator";
 
 export class CommentInterface {
   @IsString()
   @IsMongoId()
   _id: string;
+
+  @IsString()
+  @IsMongoId()
+  project: string;
+
+  @IsString()
+  @IsMongoId()
+  parent?: string;
 
   @IsString()
   @MaxLength(1000)
@@ -16,6 +24,9 @@ export class CommentInterface {
   @MinLength(4)
   @MaxLength(20)
   username: string;
+
+  @IsDate()
+  created?: Date
 
   @IsInt()
   @Min(0)
