045fe1194d18d0681f5c4d28690078ddb9307565
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-11-30 13:06:40 +0100

    COMMENT -> comment class with annotations created

--- libs/models/src/lib/comment/comment.interface.ts
+++ libs/models/src/lib/comment/comment.interface.ts
@@ -1,8 +1,27 @@
-export interface CommentInterface {
+import {IsInt, IsMongoId, IsString, MaxLength, Min, MinLength} from "class-validator";
+
+export class CommentInterface {
+  @IsString()
+  @IsMongoId()
   _id: string;
+
+  @IsString()
+  @MaxLength(1000)
   message: string;
+
+  @IsMongoId()
   owner: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsInt()
+  @Min(0)
   LikeCount: number;
+
+  @IsInt()
+  @Min(0)
   DislikeCount: number;
 }
