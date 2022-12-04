897346a6e6025b9ad14174f614eb9e745c1ddfbb
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-12-04 14:06:42 +0100

    TESTS -> Providing HttpTestingModule

--- apps/triplo/src/app/components/comment/comment.component.spec.ts
+++ apps/triplo/src/app/components/comment/comment.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CommentComponent } from './comment.component';
 import {CommentHttpService} from "../../services/comments/comment-http.service";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('CommentComponent', () => {
   let component: CommentComponent;
@@ -10,6 +11,7 @@ describe('CommentComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CommentComponent],
+      imports: [HttpClientTestingModule],
       providers: [CommentHttpService]
     }).compileComponents();
 
