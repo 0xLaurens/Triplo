2d50fcda757c35f3fd004859c977250f8af07655
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-12-04 14:31:33 +0100

    TEST -> COMMENT component mock comment added

--- apps/triplo/src/app/components/comment/comment.component.spec.ts
+++ apps/triplo/src/app/components/comment/comment.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommentComponent } from './comment.component';
 import {CommentHttpService} from "../../services/comments/comment-http.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {TuiIslandModule, TuiTagModule} from "@taiga-ui/kit";
 
 describe('CommentComponent', () => {
   let component: CommentComponent;
@@ -11,12 +12,16 @@ describe('CommentComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CommentComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, TuiTagModule, TuiIslandModule],
       providers: [CommentHttpService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CommentComponent);
     component = fixture.componentInstance;
+    component.comment = {
+      dislikeCount: 0, likeCount: 0, message: "none", owner: "none", project: "none", replyCount: 1, username: "asdf",
+      _id: "asdf"
+    }
     fixture.detectChanges();
   });
 
