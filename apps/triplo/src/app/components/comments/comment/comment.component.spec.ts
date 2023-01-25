0d0762a2440f03a03e659a949fbe3fd40887d83c
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-01-25 21:35:32 +0100

    Test -> repaired failing tests

--- apps/triplo/src/app/components/comments/comment/comment.component.spec.ts
+++ apps/triplo/src/app/components/comments/comment/comment.component.spec.ts
@@ -4,6 +4,8 @@ import { CommentComponent } from './comment.component';
 import {CommentHttpService} from "../../../services/comments/comment-http.service";
 import {HttpClientTestingModule} from "@angular/common/http/testing";
 import {TuiIslandModule, TuiTagModule} from "@taiga-ui/kit";
+import {CommonModule} from "@angular/common";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('CommentComponent', () => {
   let component: CommentComponent;
@@ -12,13 +14,14 @@ describe('CommentComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CommentComponent],
-      imports: [HttpClientTestingModule, TuiTagModule, TuiIslandModule],
+      imports: [HttpClientTestingModule, TuiTagModule, TuiIslandModule, RouterTestingModule],
       providers: [CommentHttpService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CommentComponent);
     component = fixture.componentInstance;
     component.comment = {
+      depth: 0, replies: [], updated: false,
       dislikeCount: 0, likeCount: 0, message: "none", owner: "none", project: "none", replyCount: 1, username: "asdf",
       _id: "asdf"
     }
