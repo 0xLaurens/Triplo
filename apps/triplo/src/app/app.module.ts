59d46ee2c050e8e7b117d1f940e47a3e7ff6d174
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-02-26 18:44:31 +0100

    users -> removed users page

--- apps/triplo/src/app/app.module.ts
+++ apps/triplo/src/app/app.module.ts
@@ -3,13 +3,9 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {UserListComponent} from './pages/user/user-list/user-list.component';
-import {UserDetailComponent} from './pages/user/user-detail/user-detail.component';
-import {UserEditComponent} from './pages/user/user-edit/user-edit.component';
 import {RouterModule, RouterOutlet} from '@angular/router';
 import {AppRoutingModule} from './app-routing.module';
 import {NavbarComponent} from './shared/navbar/navbar.component';
-import {HeroComponent} from './pages/home/hero/hero.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AboutModule} from './pages/about/about.module';
 import {AboutComponent} from './pages/about/about.component';
