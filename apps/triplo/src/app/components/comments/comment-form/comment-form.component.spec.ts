2e27a060d225bac6a8516a888e0885d365e67b72
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-02-26 19:29:54 +0100

    comment -> testbed

--- apps/triplo/src/app/components/comments/comment-form/comment-form.component.spec.ts
+++ apps/triplo/src/app/components/comments/comment-form/comment-form.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CommentFormComponent } from './comment-form.component';
 import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 import {TuiInputModule} from "@taiga-ui/kit";
+import {AuthHttpService} from "../../../services/authentication/auth-http.service";
+import {CommentHttpService} from "../../../services/comments/comment-http.service";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('CommentFormComponent', () => {
   let component: CommentFormComponent;
@@ -11,8 +14,8 @@ describe('CommentFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CommentFormComponent],
-      imports: [ReactiveFormsModule, TuiInputModule],
-      providers: [FormBuilder]
+      imports: [ReactiveFormsModule,HttpClientTestingModule, TuiInputModule],
+      providers: [FormBuilder, AuthHttpService, CommentHttpService]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CommentFormComponent);
