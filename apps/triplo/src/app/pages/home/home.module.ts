6975b6f2678b99f9eddc0ad20f467c346224451f
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-02-27 08:56:47 +0100

    home (hero) -> imported TuiModules

--- apps/triplo/src/app/pages/home/home.module.ts
+++ apps/triplo/src/app/pages/home/home.module.ts
@@ -1,10 +1,15 @@
 import {NgModule} from "@angular/core";
 import {HeroComponent} from "./hero/hero.component";
+import {TuiButtonModule} from "@taiga-ui/core";
+import {RouterLink} from "@angular/router";
 
 @NgModule({
   declarations: [HeroComponent],
   exports: [HeroComponent],
-  imports: [],
+  imports: [
+    TuiButtonModule,
+    RouterLink
+  ],
 })
 
 export class HomeModule {}
