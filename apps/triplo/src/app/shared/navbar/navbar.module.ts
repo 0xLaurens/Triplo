4d958876a2c54a87bc3f4f883bffe950b3e421c9
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-03-02 10:51:50 +0100

    navbar -> solid background (#52)

--- apps/triplo/src/app/shared/navbar/navbar.module.ts
+++ apps/triplo/src/app/shared/navbar/navbar.module.ts
@@ -5,18 +5,20 @@ import {RouterLink} from "@angular/router";
 import {AsyncPipe, NgIf} from "@angular/common";
 import {TuiSidebarModule} from "@taiga-ui/addon-mobile";
 import {TuiActiveZoneModule} from '@taiga-ui/cdk';
+import {TuiIslandModule} from "@taiga-ui/kit";
 
 @NgModule({
   declarations: [NavbarComponent],
   exports: [NavbarComponent],
-  imports: [
-    TuiButtonModule,
-    TuiActiveZoneModule,
-    RouterLink,
-    NgIf,
-    AsyncPipe,
-    TuiSidebarModule
-  ]
+    imports: [
+        TuiButtonModule,
+        TuiActiveZoneModule,
+        RouterLink,
+        NgIf,
+        AsyncPipe,
+        TuiSidebarModule,
+        TuiIslandModule
+    ]
 })
 
 export class NavbarModule {}
