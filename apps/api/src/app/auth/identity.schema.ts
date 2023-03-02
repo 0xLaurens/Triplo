8acd1ef956695e398d3feb6556ac1c07a1d6ed59
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-03-02 14:09:06 +0100

    project -> like button returned (#61)

--- apps/api/src/app/auth/identity.schema.ts
+++ apps/api/src/app/auth/identity.schema.ts
@@ -1,7 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import isEmail from 'validator/lib/isEmail';
-
 export type IdentityDocument = Identity & Document;
 
 @Schema()
@@ -12,10 +10,6 @@ export class Identity {
     @Prop({
         required: true,
         unique: true,
-        validate: {
-          validator: isEmail,
-          message: 'should be a valid email address',
-        }
       })
       email: string;
 }
